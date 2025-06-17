cf82483be2f669dc1bfe1ac75fe5d6017c010d89
Oleksandr <o.korotchyn9@gmail.com>
2025-06-17 18:18:44 +0200

    @

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,6 +3,7 @@ from .shop import router as shop_router
 from .crafting import router as crafting_router
 from .eat import router as eat_router
 from .use import router as use_router
+from .groups import router as group_router
 
 def register_handlers(dp):
     dp.include_router(base_router)
@@ -10,3 +11,4 @@ def register_handlers(dp):
     dp.include_router(crafting_router)
     dp.include_router(eat_router)
     dp.include_router(use_router)
+    dp.include_router(group_router)
