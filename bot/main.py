df309b89fa61dbe8c2dee79b002eb405b09b6070
Oleksandr <o.korotchyn9@gmail.com>
2025-06-17 19:00:23 +0200

    dt

--- bot/main.py
+++ bot/main.py
@@ -47,7 +47,7 @@ async def daily_reward():
     if BOT is None:
         return
 
-    now   = datetime.utcnow()
+    now   = datetime.datetime.utcnow()
     today = now.date()
 
     msgs = []        # сюди складемо строки для групи
